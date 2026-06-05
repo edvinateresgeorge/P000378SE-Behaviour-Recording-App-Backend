527fac5aea26308a97ff9d2b3f18750dca18ceb4
Megha <63230558+MeghaBhimani@users.noreply.github.com>
2026-06-05 10:48:22 +1000

    Updated the routines to add start date and time.

--- BehaviourBridgeTracks/src/main/java/com/behaviourbridgetracks/dto/RoutineRequest.java
+++ BehaviourBridgeTracks/src/main/java/com/behaviourbridgetracks/dto/RoutineRequest.java
@@ -1,16 +1,19 @@
 package com.behaviourbridgetracks.dto;
 
 import lombok.Data;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class RoutineRequest {
     private String title;
     private String category;
-    private Integer durationMinutes;
+    private LocalDateTime startDate;
     private String startTime;
     private List<Integer> repeatDays;
     private List<StepRequest> steps;
+    private LocalDateTime endDate;
 
     @Data
     public static class StepRequest {
